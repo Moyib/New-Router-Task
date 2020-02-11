19395a701ad60c3c23ba8b32fc224c9fdb5b5db8
Moyib Ayomide <mideyyadetola@gmail.com>
2020-02-11 22:23:11 +0100

    RouterTask

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ROUTER TASK OHHH!!!</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const Home = {
+  template: '<div>Home</div>'
+}
+const About = {
+  template: '<div>About</div>'
+}
+const Contact = {
+  template: '<div>Contact</div>'
+}
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+    { path: '/contact', component: Contact },
+  ]
+});
+
+new Vue({
+  router
+}).$mount('#app')
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
